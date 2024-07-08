ff7e240619b36721400162c74d1bff936c8a9c8d
RAJIV MUUDDUKUPPAM <132074085+rajivtheju@users.noreply.github.com>
2024-07-08 20:46:26 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand,
+  MDBIcon,
+} from 'mdb-react-ui-kit';
+import './Workshop.css';
+import logo from './bodhasoft-logo.png';
+export default function Navbar() {
+  return (
+    <>
+      <MDBNavbar sticky light className='Navbar'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>
+          <section className='logo'>
+                    <img src={logo}  alt='bodhasoft' className='logo-img'/>
+                    <h3 className='bodhasoft-name'>Bodhasoft</h3>
+                 </section>
+          </MDBNavbarBrand>
+          <MDBNavbarBrand href='#'>
+          <section className='username'>
+                <MDBIcon fas icon="user-alt" className='user-icon'/>
+                <p className='username-text'>username </p>
+                </section>
+          </MDBNavbarBrand>
+
+        </MDBContainer>
+      </MDBNavbar>
+    </>
+  );
+}
