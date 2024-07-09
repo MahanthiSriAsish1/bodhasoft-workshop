c9db2ab50371af8711e0627f51f1e9dfd528caf4
RAJIV MUUDDUKUPPAM <132074085+rajivtheju@users.noreply.github.com>
2024-07-09 23:05:50 +0530

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './Dashboard.css'
+import { MDBIcon } from 'mdb-react-ui-kit'
+const Dashboard = () => {
+  return (
+    <div >
+     <Navbar/>
+     <div className='box'>
+     <div className='d-container'>
+<section className='d-box'>
+<MDBIcon fas icon="file-code" className='d-icon' />
+  <h3 className='d-head'>Problem Statement</h3> </section>
+<section className='d-box'>
+<MDBIcon fas icon="puzzle-piece" className='d-icon'  />
+  <h3 className='d-head'>Workshop Area</h3></section>
+<section className='d-box'>
+<MDBIcon fas icon="book" className='d-icon' />
+  <h3 className='d-head'>Working Material</h3></section>
+     </div>
+     </div>
+    </div>
+  )
+}
+
+export default Dashboard
