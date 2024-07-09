259f4032097f6057a97451c01917128233bbc97a
RAJIV MUUDDUKUPPAM <132074085+rajivtheju@users.noreply.github.com>
2024-07-09 23:15:31 +0530

    Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -5,21 +5,23 @@ import { MDBIcon } from 'mdb-react-ui-kit'
 const Dashboard = () => {
   return (
     <div >
-     <Navbar/>
-     <div className='box'>
-     <div className='d-container'>
-<section className='d-box'>
-<MDBIcon fas icon="file-code" className='d-icon' />
-  <h3 className='d-head'>Problem Statement</h3> </section>
-<section className='d-box'>
-<MDBIcon fas icon="puzzle-piece" className='d-icon'  />
-  <h3 className='d-head'>Workshop Area</h3></section>
-<section className='d-box'>
-<MDBIcon fas icon="book" className='d-icon' />
-  <h3 className='d-head'>Working Material</h3></section>
-     </div>
-     </div>
+      <Navbar />
+      <div className='d-container'>
+        <section className='d-box'>
+          <MDBIcon fas icon="file-code" className='d-icon' />
+          <h3 className='d-head'>Problem Statement</h3>
+        </section>
+        <section className='d-box'>
+          <MDBIcon fas icon="puzzle-piece" className='d-icon' />
+          <h3 className='d-head'>Workshop Area</h3>
+        </section>
+        <section className='d-box'>
+          <MDBIcon fas icon="book" className='d-icon' />
+          <h3 className='d-head'>Working Material</h3>
+        </section>
+      </div>
     </div>
+
   )
 }
 
