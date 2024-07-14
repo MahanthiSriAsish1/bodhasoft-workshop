c2605a6165e627ebd49c05ea25be4a19b531491d
MahanthiSriAsish1 <110473671+MahanthiSriAsish1@users.noreply.github.com>
2024-07-14 12:52:13 +0530

    adding backend

---
+++ Config/MongoConfig.js
@@ -0,0 +1,82 @@
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://asish:Asish2002@cluster0.7uwd7p7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+async function connectClient() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+const listDatabases = async () => {
+  const databaseList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databaseList.databases.forEach((db) => console.log(` - ${db.name}`));
+};
+
+const insertDoc = async (dbName, collectionName, doc) => {
+  try {
+    const collection = client.db(dbName).collection(collectionName);
+
+    // Check if document already exists based on name, college, phone, and email
+    const existingDoc = await collection.findOne({
+      name: doc.name,
+      college: doc.college,
+      phone: doc.phone,
+      email: doc.email,
+    });
+
+    if (existingDoc) {
+      return "Document already exists";
+    }
+
+    const result = await collection.insertOne(doc);
+    return `New document inserted with the following id: ${result.insertedId}`;
+  } catch (error) {
+    console.error("Error inserting document:", error);
+    throw error;
+  }
+};
+
+const findDocs = async (dbName, collectionName) => {
+  const collection = client.db(dbName).collection(collectionName);
+  const results = await collection.find({}).toArray();
+  console.log("Found documents:", results);
+  return results;
+};
+
+const updateDoc = async (dbName, collectionName, filter, update) => {
+  const result = await client
+    .db(dbName)
+    .collection(collectionName)
+    .updateOne(filter, { $set: update });
+  console.log(
+    `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
+  );
+};
+
+const deleteDoc = async (dbName, collectionName, filter) => {
+  const result = await client
+    .db(dbName)
+    .collection(collectionName)
+    .deleteOne(filter);
+  console.log(`Deleted ${result.deletedCount} document(s)`);
+};
+
+module.exports = {
+  client,
+  connectClient,
+  listDatabases,
+  insertDoc,
+  findDocs,
+  updateDoc,
+  deleteDoc,
+};
---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const {
+  client,
+  connectClient,
+  insertDoc,
+  findDocs,
+  updateDoc,
+  deleteDoc,
+} = require("./Config/MongoConfig");
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Update this to restrict to your specific client URL if needed
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+let problemStatementDisabled = true;
+let workshopAreaDisabled = true;
+
+// Socket.IO setup
+io.on("connection", (socket) => {
+  console.log("a new user has connected", socket.id);
+  socket.emit("messageFromServer", {
+    message: "Hello from the server!",
+  });
+
+  // Emit an event to all connected clients (including the newly connected client)
+  io.emit("broadcastMessage", {
+    message: "A new user has joined the chat!",
+  });
+
+  socket.on("sendMessage", (data) => {
+    console.log("Received message from client:", data.message);
+    // Broadcast the received message to all clients
+    io.emit("newMessage", {
+      message: data.message,
+    });
+  });
+
+  socket.on("unlockProblemStatement", () => {
+    problemStatementDisabled = false;
+    io.emit("enableProblemStatement");
+  });
+
+  socket.on("unlockWorkshopArea", () => {
+    workshopAreaDisabled = false;
+    io.emit("enableWorkshopArea");
+  });
+
+  socket.on("unlockQuestion", ({ questionId }) => {
+    console.log(`Unlocked Question: ${questionId}`);
+    io.emit("unlockQuestion", questionId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+
+// Middleware setup
+app.use(cors());
+app.use(express.json()); // For parsing application/json
+
+// Connect to MongoDB
+connectClient().catch(console.error);
+
+// Serve static files from the 'public' directory
+app.use(express.static("public"));
+
+// MongoDB API routes
+app.post("/api/insert", async (req, res) => {
+  const newDoc = req.body;
+  try {
+    const result = await insertDoc("Workshop_Data", "Student_Details", newDoc);
+      res.status(201).send(result);
+  } catch (error) {
+    res.status(500).send("Error inserting document: " + error.message);
+  }
+});
+
+app.get("/api/find", async (req, res) => {
+  try {
+    const results = await findDocs("Workshop_Data", "Student_Details");
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(500).send("Error finding documents: " + error.message);
+  }
+});
+
+app.put("/api/update", async (req, res) => {
+  const { filter, update } = req.body;
+  try {
+    await updateDoc("Workshop_Data", "Student_Details", filter, update);
+    res.status(200).send("Document updated");
+  } catch (error) {
+    res.status(500).send("Error updating document: " + error.message);
+  }
+});
+
+app.delete("/api/delete", async (req, res) => {
+  const filter = req.body;
+  try {
+    await deleteDoc("Workshop_Data", "Student_Details", filter);
+    res.status(200).send("Document deleted");
+  } catch (error) {
+    res.status(500).send("Error deleting document: " + error.message);
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server started at PORT Number: ${PORT}`);
+});
